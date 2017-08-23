a3307a32fffcfeb8a9d45ee6cc963d4950f9fda8
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-23 17:00:44 +0800

    added extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var isProd = process.env.NODE_ENV === 'production';
-var cssProd = ['style-loader', 'css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: ['css-loader', 'sass-loader']
+});
 var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 var cssConfig = isProd ? cssProd : cssDev;
-
 
 module.exports = {
     entry: './src/app.jsx',
@@ -20,14 +23,15 @@ module.exports = {
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin(
-        {
+    plugins: [
+        new HtmlWebpackPlugin({
             title: "Webpack 101",
             minify: {
                 collapseWhitespace: isProd
             },
             template: './src/index.ejs'
-        }
-    )]
+        }),
+        new ExtractTextPlugin('app.css')
+    ]
 
 }
