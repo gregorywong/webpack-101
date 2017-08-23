73dd4da07eb0c0529f41d181838a8b5a817dc596
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-23 16:33:53 +0800

    set up webpack and html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: "Webpack 101",
+            template: './src/index.ejs'
+        }
+    )]
+
+}
