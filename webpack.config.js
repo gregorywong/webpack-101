3abe3dda175ef50ee97e52ab958908da6daa3e4b
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-23 16:48:41 +0800

    load css and sass

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
     plugins: [new HtmlWebpackPlugin(
         {
             title: "Webpack 101",
